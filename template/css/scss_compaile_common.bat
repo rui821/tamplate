942aadb2cae1b4b830aaa83511f8c44520ec2292
rui821 <81800967+rui821@users.noreply.github.com>
2021-12-18 22:11:39 +0900

    Add files via upload

---
+++ template/css/scss_compaile_common.bat
@@ -0,0 +1,2 @@
+sass --watch common.scss:common.css --style expanded && ^
+sass --watch style.scss:style.css --style expanded
---
+++ template/css/scss_compaile_style.bat
@@ -0,0 +1,4 @@
+
+sass --watch style.scss:style.css --style expanded && ^
+sass --watch common.scss:common.css --style expanded 
+
